f02d2507fe1ab6cf932fecd5fddc28c4716138b1
beholderface <arthur@thibault.org>
2024-09-07 16:20:52 -0400

    aaaaaaaaaaaaaaaaaaaaaa

--- forge/src/main/java/net/beholderface/ephemera/forge/EphemeraClientForge.java
+++ forge/src/main/java/net/beholderface/ephemera/forge/EphemeraClientForge.java
@@ -1,15 +1,19 @@
 package net.beholderface.ephemera.forge;
 
 import at.petrak.hexcasting.client.RegisterClientStuff;
+import at.petrak.hexcasting.forge.ForgeHexClientInitializer;
 import net.beholderface.ephemera.CopiedSlateRenderer;
 import net.beholderface.ephemera.Ephemera;
 import net.beholderface.ephemera.EphemeraClient;
 import net.beholderface.ephemera.registry.EphemeraBlockRegistry;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -17,15 +21,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 /**
  * Forge client loading entrypoint.
  */
+@Mod.EventBusSubscriber
 public class EphemeraClientForge {
     public static void init(FMLClientSetupEvent event) {
         EphemeraClient.init();
-
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        bus.addListener((EntityRenderersEvent.RegisterRenderers evt)->{
-            Ephemera.LOGGER.info("Registering ephemera renderers BUT HOPEFULLY WORKING THIS TIME");
-            evt.registerBlockEntityRenderer(EphemeraBlockRegistry.SNEAKY_SLATE_ENTITY.get(), CopiedSlateRenderer::new);
-        });
     }
 
     @SubscribeEvent
--- forge/src/main/java/net/beholderface/ephemera/forge/EphemeraForge.java
+++ forge/src/main/java/net/beholderface/ephemera/forge/EphemeraForge.java
@@ -1,9 +1,13 @@
 package net.beholderface.ephemera.forge;
 
+import at.petrak.hexcasting.forge.ForgeHexClientInitializer;
 import dev.architectury.platform.forge.EventBuses;
 import net.beholderface.ephemera.Ephemera;
 import net.beholderface.ephemera.forge.ForgePacketHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
